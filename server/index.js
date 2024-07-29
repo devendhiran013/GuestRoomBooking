30b6182f3192e28496c32030cfcfa7ea3fe73558
Devendhiran <devsavi07@gmail.com>
2024-07-29 20:28:46 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,41 +1,31 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-require("dotenv").config();
+const dotenv = require("dotenv").config();
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth");
-const listingRoutes = require("./routes/listing");
-const bookingRoutes = require("./routes/booking");
-const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth.js")
+const listingRoutes = require("./routes/listing.js")
+const bookingRoutes = require("./routes/booking.js")
+const userRoutes = require("./routes/user.js")
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// Root Route
-app.get("/", (req, res) => {
-  res.send("Welcome to the Guestroom Booking API");
-});
 
-// Routes
-app.use("/auth", authRoutes);
-app.use("/properties", listingRoutes);
-app.use("/bookings", bookingRoutes);
-app.use("/users", userRoutes);
 
-// Error Handling Middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send({ error: "Internal Server Error" });
-});
+/* ROUTES */
+app.use("/auth", authRoutes)
+app.use("/properties", listingRoutes)
+app.use("/bookings", bookingRoutes)
+app.use("/users", userRoutes)
 
-// Mongoose Setup
-const PORT = process.env.PORT || 3001;
+/* MONGOOSE SETUP */
+const PORT = 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
-    dbName: "Dream_House",
+    dbName: "Dream_Nest",
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -43,5 +33,3 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
   .catch((err) => console.log(`${err} did not connect`));
-
-module.exports = app; // Export the app for testing

9490c78545deca9c115755a6e478d995ad117daa
ASHS27 <ashoksekar308@gmail.com>
2024-09-20 10:32:27 +0530

    updated

--- server/api/index.js
+++ server/api/index.js
@@ -4,10 +4,10 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth.js")
-const listingRoutes = require("./routes/listing.js")
-const bookingRoutes = require("./routes/booking.js")
-const userRoutes = require("./routes/user.js")
+const authRoutes = require("../routes/auth.js")
+const listingRoutes = require("../routes/listing.js")
+const bookingRoutes = require("../routes/booking.js")
+const userRoutes = require("../routes/user.js")
 
 app.use(cors());
 app.use(express.json());
